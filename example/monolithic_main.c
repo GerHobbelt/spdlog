353a16f79c8801da0d99a75bfe4c3bd247e8abbb
Ger Hobbelt <ger@hobbelt.com>
2024-01-23 18:55:58 +0100

    fixes for monolithic build mode.

---
+++ example/monolithic_main.c
@@ -0,0 +1,17 @@
+
+#define BUILD_MONOLITHIC 1
+#include "monolithic_examples.h"
+
+#define USAGE_NAME   "threadpool"
+
+#include "monolithic_main_internal_defs.h"
+
+MONOLITHIC_CMD_TABLE_START()
+{ "async_bench", {.fpp = spdlog_async_bench_main } },
+{ "bench", { .fpp = spdlog_bench_main } },
+{ "formatter_bench", { .fpp = spdlog_formatter_bench_main } },
+{ "latency_bench", { .fpp = spdlog_latency_bench_main } },
+{ "example", { .fpp = spdlog_example_main } },
+MONOLITHIC_CMD_TABLE_END();
+
+#include "monolithic_main_tpl.h"
