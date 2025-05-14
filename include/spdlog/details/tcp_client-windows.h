6f8ef72ac753b743debc582a62fddb91cdc1319f
Ger Hobbelt <ger@hobbelt.com>
2025-05-14 18:48:59 +0200

    fixes some extremely obnoxious and hard-to-diagnose errors (this time, we dug them out using the /P render-to-preprocessed-file compiler mode plus a few grep incantations to find the starting point of this crap, which recurs again and again because all eedjits follow MS advice about LEAN_AND_MEAN and let userland cope with the mess; ditto for `#include <windows.h>` in your library header file and NOT loading `winsock2.h` (duh!), but then userland code loads the latter one after as, hey, it's all part of some bigger scheme now! !@#$%^&*(*&^%@#$&#$!!!
           #if 0		               // [GHo: WIN32_LEAN_AND_MEAN is cute, but no cigar: when header files such as these do this and are themselves loaded in sourcefiles of larger applications/userland code, things can break in very hard-to-diagnose ways! So... DON'T!
           #define WIN32_LEAN_AND_MEAN
           #endif
    
           #include <winsock2.h>   // [GHo] fixes issues elsewhere, e.g. spdlog, when this header file is loaded before another, which loads winsock2.h or (*shudder*) winsock.h, e.g. windows.h: you'll get clashes in preprocessor defines in ws2def.h vs. winsock2.h  :-((
           #include <ws2tcpip.h>   // [GHo] optional; if you want, grab it. This one doesn't cause the trouble windows.h & winsock2.h/ws2def.h do, as a pair.  >-((
           #include <windows.h>

--- include/spdlog/details/tcp_client-windows.h
+++ include/spdlog/details/tcp_client-windows.h
@@ -3,7 +3,10 @@
 
 #pragma once
 
+#if 0		               // [GHo: WIN32_LEAN_AND_MEAN is cute, but no cigar: when header files such as these do this and are themselves loaded in sourcefiles of larger applications/userland code, things can break in very hard-to-diagnose ways! So... DON'T!
 #define WIN32_LEAN_AND_MEAN
+#endif
+
 // tcp client helper
 #include <spdlog/common.h>
 #include <spdlog/details/os.h>
@@ -11,9 +14,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <windows.h>
-#include <winsock2.h>
+#include <winsock2.h>   // [GHo] fixes issues elsewhere, e.g. spdlog, when this header file is loaded before another, which loads winsock2.h or (*shudder*) winsock.h, e.g. windows.h: you'll get clashes in preprocessor defines in ws2def.h vs. winsock2.h  :-((
 #include <ws2tcpip.h>
+#include <windows.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Mswsock.lib")
--- include/spdlog/details/windows_include.h
+++ include/spdlog/details/windows_include.h
@@ -4,8 +4,11 @@
     #define NOMINMAX  // prevent windows redefining min/max
 #endif
 
+#if 0		               // [GHo: WIN32_LEAN_AND_MEAN is cute, but no cigar: when header files such as these do this and are themselves loaded in sourcefiles of larger applications/userland code, things can break in very hard-to-diagnose ways! So... DON'T!
 #ifndef WIN32_LEAN_AND_MEAN
     #define WIN32_LEAN_AND_MEAN
 #endif
+#endif
 
+#include <winsock2.h>   // [GHo] fixes issues elsewhere, e.g. spdlog, when this header file is loaded before another, which loads winsock2.h or (*shudder*) winsock.h, e.g. windows.h: you'll get clashes in preprocessor defines in ws2def.h vs. winsock2.h  :-((
 #include <windows.h>
