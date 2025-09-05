e585fd4627773ba58ce087b992504035976524ba
John Armstrong <jarmstrong@esri.com>
2025-09-05 12:20:38 -0700

    Fix windows build error

--- spdlog.lua
+++ spdlog.lua
@@ -40,7 +40,13 @@ if (_PLATFORM_MACOS) then
 end
 
 if (_PLATFORM_WINDOWS) then
+  defines {
+    "FMT_UNICODE=0",
+  }
 end
 
 if (_PLATFORM_WINUWP) then
+  defines {
+    "FMT_UNICODE=0",
+  }
 end
